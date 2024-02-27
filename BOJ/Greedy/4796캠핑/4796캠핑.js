ed34de0812697920c7fc8ca78e4a2233abe20d69
Theo Jin <jhb_august@naver.com>
2024-02-28 03:00:55 +0900

    first commit

---
+++ BOJ/Greedy/4796캠핑/4796캠핑.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\r\n");
+
+
+const realInput = input.slice(0, -1)
+const answer = realInput.map((a, i) => {
+    const [L, P, V] = a.split(' ').map(Number);
+    let share = Math.floor(V / P)
+    let remain = V%P > L ? L : V%P;
+    return `Case ${i + 1}: ${L * share + remain}`
+}).join("\n")
+console.log(answer)
